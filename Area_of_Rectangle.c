08f035cd161ce591442030ec1e3e3a34d95e6d4f
Rushil Mahajan <rushilmahajan39@gmail.com>
2026-02-01 12:25:06 +0530

    Add files via upload
    This file contains code to find the area of a rectangle when the sides are given and when they are entered by the user.

---
+++ Area_of_Rectangle.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+int main(){
+// AREA OF RECTANGLE
+// if length and width is given;
+
+
+
+
+int length,width;
+length=5;
+width=2;
+int area = length * width;
+printf("Area of rectangle is %d",area);
+
+
+
+
+
+
+
+//if length and width is given by user;
+
+
+
+int length,width;
+printf("Enter the Length : ");
+scanf("%d",&length);
+printf("ENter the Width : ");
+scanf("%d",&width);
+int area = length * width;
+printf("Area of rectangle is %d",area);
+
+
+
+
+return 0;
+}
